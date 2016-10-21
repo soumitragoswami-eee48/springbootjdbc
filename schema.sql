ec1b9897b44e4e67858508824336e86b7e84a161
Soumitra Goswami <sgoswami@broadsoft.com>
2016-10-21 16:32:21 +0530

    added schema file

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS student (id INT, `name` VARCHAR(20), roll INT);
+INSERT INTO springbootdb.student (id, `name`, roll) 
+	VALUES (1, 'ElNino', 1);
+INSERT INTO springbootdb.student (id, `name`, roll) 
+	VALUES (2, 'laNina', 2);
+INSERT INTO springbootdb.student (id, `name`, roll) 
+	VALUES (3, 'Ar', 3);
